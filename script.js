de7d63116de8f32495938c99a065d4e8f7c52818
Nicole Stawikowski <nicole.stawikowski@gmail.com>
2021-06-04 16:12:20 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+//cached things
+
+let btnPlus = document.getElementById("increment") 
+let btnMinus = document.getElementById("decrement")
+
+//event listeners
+
+
+btnPlus.addEventListener("click", btnPlusClick ) 
+btnMinus.addEventListener("click", btnMinusClick)
+
+//functions
+
+function btnPlusClick () {
+    let count = inputValue.value;
+    let theInput = 0
+    console.log(count)
+
+}
+
+function btnMinusClick () {
+    let count = inputValue.value;
+    let theInput = 0;
+    console.log(count)
+    
+    theInput --
+    inputValue.innterText = 
+}
